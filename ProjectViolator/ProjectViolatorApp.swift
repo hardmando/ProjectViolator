d8a94750bcd14d8535eeb1eef1ea120fd6e6faf4
Anton Tovshchyk <totonn777@gmail.com>
2023-05-02 16:18:46 -0500

    Initial Commit

---
+++ ProjectViolator/ProjectViolatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectViolatorApp.swift
+//  ProjectViolator
+//
+//  Created by Hardmando on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectViolatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
