4c44ab27b2141c4e03fdfdcb59e46ba05cb844b5
Anton Tovshchyk <totonn777@gmail.com>
2023-05-03 17:02:04 -0500

    Added gradient to Month Text

--- ProjectViolator/ContentView.swift
+++ ProjectViolator/ContentView.swift
@@ -7,14 +7,38 @@
 
 import SwiftUI
 
+extension Date {
+    var month: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM"
+        return dateFormatter.string(from: self)
+    }
+}
+
+
+let date = Date()
+let monthString = date.month
+
+let textGrad = LinearGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+
+
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        cardContainer {
+            let accentColor = Color(red: 1, green: 0.5, blue: 0.5)
+            Text(monthString)
+                .font(.title)
+                .position(x: 185, y:20)
+                .foregroundStyle(textGrad)
+            
+            VStack{
+                
+            }
+            
         }
+        
         .padding()
     }
 }
